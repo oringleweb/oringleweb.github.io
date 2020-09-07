7587e53fa9dff9a3663e05d36a2de03d0c6bf10e
Dominik Juriga <uJexxi@gmail.com>
2020-09-08 00:53:02 +0200

    Updates

---
+++ precache-manifest.cf5be6aa0b9fa971d92d41afe48e0787.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8fdcd8a5f634227c9ad4751c7882c84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f06fba17f7d1092346ac",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "d9fa7a33180ca579cad5",
+    "url": "/static/css/main.cdd3f4b5.chunk.css"
+  },
+  {
+    "revision": "f06fba17f7d1092346ac",
+    "url": "/static/js/2.c5bd0fdb.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/static/js/2.c5bd0fdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9fa7a33180ca579cad5",
+    "url": "/static/js/main.c27ab448.chunk.js"
+  },
+  {
+    "revision": "a415b88554d1a237b621",
+    "url": "/static/js/runtime-main.0da32d9f.js"
+  }
+]);
